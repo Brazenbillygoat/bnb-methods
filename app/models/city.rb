974d50a3b6bfd6fee18b32cbcc21bfe24970f00c
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-20 18:23:52 -0400

    Defined a method to find the highest ratio of reservations to listings

--- app/models/city.rb
+++ app/models/city.rb
@@ -18,8 +18,29 @@ class City < ActiveRecord::Base
 
   def self.highest_ratio_res_to_listings
 
+    ratio_hash = {}
+    cities = self.group(:name)
+
+
+    cities.each do |city|
+      count = 0
+      listings = city.listings
+
+      reservation_count = listings.map do |listing|
+        listing.reserved_dates.count
+      end
+
+      if reservation_count[count] == 0 || city.listings.count == 0
+        next
+      else
+        ratio_hash[city] = (city.listings.count / reservation_count[count])
+      end
+      
+      count += 1
+    end
+    ratio_hash.max_by{ |k, v| v }[0]
   end
-      # listing.neighborhood.city 
+      
    
 
 
