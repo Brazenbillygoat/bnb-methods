78984d8dc5b070afa950998583c47ef25ee1fa1d
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-20 21:47:29 -0400

    Callbacks still need debugging

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,11 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
+
+  # before_destroy :change_to_guest
+  # before_save :change_to_host
 
     def reserved_dates
        self.reservations.map do |res|
@@ -13,7 +17,16 @@ class Listing < ActiveRecord::Base
     end
 
 
-    #I am a change
+    private
+
+    def change_to_host
+      self.host = true
+    end
+
+    def change_to_guest
+      self.host.listings.empty? ? self.host.host = false : self.host.host = true
+
+    end
 
 end
 
